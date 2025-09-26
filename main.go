726b1166f5cd32eb0b02d292705985d0e92c80fe
devhenryno <devhenryno@gmail.com>
2025-09-26 15:34:08 +0100

    setup

---
+++ main.go
@@ -0,0 +1,15 @@
+package main 
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+	var myName string = "John Doe"
+	const mySecondName string = "jane Doe"
+
+	myThirdname := "Bob Doe"
+	fmt.Println(myName)
+	fmt.Println(mySecondName)
+	fmt.Println(myThirdname)
+
+}
